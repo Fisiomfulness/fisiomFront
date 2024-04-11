f00f35f2d7e75212b6ff02ff353de4fc0796c073
Kevin Mamani <kevin72309@gmail.com>
2024-04-11 18:55:28 -0300

    feat/fix: pagination for blog cards / responsive and sidebar styles

--- src/components/Blog/Publication.jsx
+++ src/components/Blog/Publication.jsx
@@ -3,14 +3,19 @@ import { truncateText, dateFormatter } from '@/utils/helpers';
 
 function Publication({ data }) {
   return (
-    <div className="w-full flex gap-5 items-center">
-      <Image isBlurred width={60} src={data.image} alt="NextUI Album Cover" />
-      <div className="flex flex-col grow">
-        <h2 className="text-[0.85rem] mb-0">{data.title}</h2>
-        <p className="text-[0.75rem] mt-[0.1rem]">
+    <div className="size-fit grid grid-cols-[25%,auto] gap-3 sm:gap-5">
+      <Image
+        isBlurred
+        src={data.image}
+        alt="Blog picture"
+        className="h-full min-h-[80px] object-cover rounded-md object-center lg:min-h-[60px]"
+      />
+      <div className="flex flex-col">
+        <h2 className="text-[0.85rem] mb-0 capitalize">{data.title}</h2>
+        <p className="text-[0.75rem] mt-[0.1rem] grow overflow-y-auto">
           {truncateText(data.text, 8)}
         </p>
-        <span className="text-[0.65rem] mt-[0.3rem]">
+        <span className="text-[0.65rem] mt-[0.3rem] justify-self-end">
           {dateFormatter(data.createdDate)}
         </span>
       </div>
