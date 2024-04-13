0d21aa4d4a0e0a33202255a6ba7fb3f208e15815
Kevin Mamani <kevin72309@gmail.com>
2024-04-13 10:23:35 -0300

    feat: create blog comment form

--- src/components/Blog/detail/BlogDetail.jsx
+++ src/components/Blog/detail/BlogDetail.jsx
@@ -1,7 +1,7 @@
 'use client';
-import { Image, Button } from '@nextui-org/react';
-import Rating from '@/components/Blog/Detail/Rating';
+import { Image } from '@nextui-org/react';
 import ScrollBlog from './ScrollBlog';
+import CommentForm from './CommentForm';
 
 const BlogDetail = ({ data }) => {
   return (
@@ -15,25 +15,7 @@ const BlogDetail = ({ data }) => {
         <h1 className="capitalize">{data.title}</h1>
         <h5 className="mb-7 leading-relaxed grow">{data.text}</h5>
         <ScrollBlog />
-        <div className="bg-[#D8EEF8] flex flex-col justify-between mt-3 md:mt-7 sm:grid sm:grid-cols-[2fr,2.5fr,1fr]">
-          <div className="flex items-center gap-2 justify-between flex-grow m-0 p-4 md:px-8 md:py-4 sm:flex-col lg:flex-row">
-            <h4 className="m-0 text-secondary">Valoración</h4>
-            <div className="flex items-center m-0">
-              <Rating />
-            </div>
-          </div>
-          <input
-            className="outline-secondary-400 flex-grow font-medium placeholder:text-gray-600 p-4 placeholder:italic rounded-none sm:mb-0 bg-zinc-100 md:px-6"
-            type="text"
-            placeholder="agregar un comentario..."
-          />
-          <Button
-            color="primary"
-            className="rounded-none p-4 !size-full bg-primary-500"
-          >
-            Enviar
-          </Button>
-        </div>
+        <CommentForm blogId={data._id} />
       </div>
     </div>
   );
