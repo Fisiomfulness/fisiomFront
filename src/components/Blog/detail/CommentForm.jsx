fc9e95941bff5c787cad197ebee019b298a35661
Jackira01 <s4bu3z0@gmail.com>
2024-04-16 10:48:19 -0500

    bug import fix @/components/Blog/detail/Rating;

--- src/components/Blog/detail/CommentForm.jsx
+++ src/components/Blog/detail/CommentForm.jsx
@@ -1,25 +1,25 @@
-import { Button } from '@nextui-org/react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { createComment } from '@/services/comments';
-import * as Yup from 'yup';
-import Rating from '@/components/Blog/Detail/Rating';
-import toast from 'react-hot-toast';
+import { Button } from "@nextui-org/react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { createComment } from "@/services/comments";
+import * as Yup from "yup";
+import Rating from "@/components/Blog/detail/Rating";
+import toast from "react-hot-toast";
 
 const initialValues = {
   rating: 0,
-  content: '',
+  content: "",
 };
 
 const commentSchema = Yup.object({
-  rating: Yup.number().required().min(1, 'Debe puntuar el blog').max(5),
+  rating: Yup.number().required().min(1, "Debe puntuar el blog").max(5),
   content: Yup.string()
-    .required('Requerido')
-    .min(3, 'Al menos 3 caracteres')
-    .max(100, 'Limite: 100 caracteres'),
+    .required("Requerido")
+    .min(3, "Al menos 3 caracteres")
+    .max(100, "Limite: 100 caracteres"),
 });
 
 // ! TODO: DB ID HARDCODED. CHANGE FOR THE ACTUAL SESSION ID
-const userId = '661df9cf4d019890d80057ac';
+const userId = "661df9cf4d019890d80057ac";
 
 const CommentForm = ({ blogId }) => {
   const handleSubmit = async (values, { resetForm }) => {
@@ -27,10 +27,10 @@ const CommentForm = ({ blogId }) => {
       const finalComment = { ...values, sender_id: userId, blog_id: blogId };
       await createComment(finalComment);
       resetForm();
-      toast.success('Comentario añadido correctamente!');
+      toast.success("Comentario añadido correctamente!");
     } catch (error) {
       console.error(error);
-      toast.error('Oops! Vuelva a intentarlo');
+      toast.error("Oops! Vuelva a intentarlo");
     }
   };
 
@@ -44,7 +44,7 @@ const CommentForm = ({ blogId }) => {
         <Form className="bg-[#D8EEF8] flex flex-col justify-between mt-3 md:mt-7 sm:grid sm:grid-cols-[2fr,2.5fr,1fr]">
           <div
             className={`flex flex-col gap-2 justify-between m-0 p-4 md:px-8 md:py-4 ${
-              errors.rating ? 'lg:pb-[0.4rem]' : 'md:pb-4'
+              errors.rating ? "lg:pb-[0.4rem]" : "md:pb-4"
             }`}
           >
             <div className="flex gap-2 items-center justify-between w-full sm:flex-col min-[1050px]:flex-row">
@@ -63,7 +63,7 @@ const CommentForm = ({ blogId }) => {
             <Field
               aria-label="comentario"
               className={`outline-secondary-400 flex-grow font-medium placeholder:text-gray-600 p-4 placeholder:italic rounded-none sm:mb-0 bg-zinc-100 md:px-6 ${
-                errors.content && touched.content ? 'pb-8' : 'sm:pb-4'
+                errors.content && touched.content ? "pb-8" : "sm:pb-4"
               }`}
               name="content"
               type="text"
