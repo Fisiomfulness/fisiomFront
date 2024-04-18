927b3f9a391f7f50fb053d4f382c3a2e3c22641b
Kevin Mamani <kevin72309@gmail.com>
2024-04-18 19:28:55 -0300

    blogs feat/style: connection of comments with database / responsive improvement for large title and text blogs

--- src/components/Blog/BlogCards.jsx
+++ src/components/Blog/BlogCards.jsx
@@ -1,6 +1,5 @@
 import { useRouter } from 'next/navigation';
 import { Pagination, Card, CardHeader, Image } from '@nextui-org/react';
-import { truncateText } from '@/utils/helpers';
 import { getBlogs } from '@/services/blogs';
 
 export default function BlogCards({
@@ -20,38 +19,36 @@ export default function BlogCards({
     });
   };
 
+  // ! TODO: MODIFY FOR LARGE TITLE AND DESCRIPTION
+  // ! ALSO SIDEBAR.
   return (
-    <div className="flex flex-col items-center gap-5">
+    <div className="w-full flex flex-col items-center gap-5">
       {blogs.length > 0 ? (
         <>
-          <div className="xl:h-[992px] justify-center size-fit gap-x-6 gap-y-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3">
+          <div className="xl:h-[1020px] size-fit gap-x-6 gap-y-8 grid sm:grid-cols-2 md:grid-cols-3">
             {blogs.map((blog) => (
-              <div
+              <Card
                 key={blog._id}
-                className="col-span-1 flex flex-col items-center h-auto rounded-md overflow-hidden"
+                className="w-full grid grid-rows-[60%,auto] !max-w-[300px] h-[318px] rounded-lg shadow-md overflow-hidden"
               >
-                <Card className="w-full max-w-[300px] h-10rem rounded-none shadow-lg sm:w-full">
-                  <Image
-                    removeWrapper
-                    alt={`${blog.title} picture`}
-                    className="z-0 w-full h-48 object-cover rounded-none"
-                    src={blog.image}
-                  />
-                  <CardHeader
-                    onClick={() => router.push(`/blog/${blog._id}`)}
-                    className="relative z-10 w-full flex flex-col items-start bg-[#D8EEF8] h-auto p-4 cursor-pointer rounded-none"
-                  >
-                    <div>
-                      <p className="text-xs lg:text-sm text-black uppercase font-bold">
-                        {blog.title}
-                      </p>
-                      <h6 className="text-xs lg:text-sm text-black font-medium overflow-hidden ">
-                        {truncateText(blog.text, 15)}
-                      </h6>
-                    </div>
-                  </CardHeader>
-                </Card>
-              </div>
+                <Image
+                  removeWrapper
+                  alt={`${blog.title} picture`}
+                  className="size-full object-cover rounded-none"
+                  src={blog.image}
+                />
+                <CardHeader
+                  onClick={() => router.push(`/blog/${blog._id}`)}
+                  className="overflow-hidden grid grid-rows-[max-content,auto] gap-1 items-start bg-[#D8EEF8] cursor-pointer rounded-none"
+                >
+                  <h3 className="truncate w-full text-xs m-0 lg:text-sm text-black uppercase font-bold">
+                    {blog.title}
+                  </h3>
+                  <p className="text-xs h-full line-clamp-5 lg:text-sm lg:line-clamp-4 text-black font-medium">
+                    {blog.text}
+                  </p>
+                </CardHeader>
+              </Card>
             ))}
           </div>
           <Pagination
