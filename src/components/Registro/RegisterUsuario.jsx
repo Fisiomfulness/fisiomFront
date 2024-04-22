24bf95effbb44398081c1648c42055f20d5c93f6
Jackira01 <s4bu3z0@gmail.com>
2024-04-21 21:21:56 -0500

    udpate register component

--- src/components/Registro/RegisterUsuario.jsx
+++ src/components/Registro/RegisterUsuario.jsx
@@ -1,156 +1,199 @@
 "use client";
 
-import React, { useState } from "react";
-import { CustomButton, CustomInput } from "@/features/ui";
-import { registerUserForm } from "@/services/register";
+import React, { useState, useCallback } from "react";
 import toast from "react-hot-toast";
 import Link from "next/link";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
+import { CustomButton, CustomInput } from "@/features/ui";
+import { registerUserForm } from "@/services/register";
+import { actualMinDate } from "@/utils/helpers";
+import { useDropzone } from "react-dropzone";
+import { Button, Select, SelectItem } from "@nextui-org/react";
+
+//#region Formik config
+const initialValues = {
+  name: "",
+  phone: "",
+  email: "",
+  dateOfBirth: "",
+  password: "",
+  repitPass: "",
+  gender: "Masculino",
+};
+
+const registerSchemaValidation = Yup.object({
+  name: Yup.string()
+    .required("El nombre es requerido")
+    .min(3, "El nombre debe tener al menos 3 caracteres"),
+  phone: Yup.string().required("Numero telefonico requerido"),
+  email: Yup.string()
+    .required("Correo electronico requerido")
+    .email("No es un email"),
+  dateOfBirth: Yup.date()
+    .required("La fecha de nacimiento es requerida")
+    .max(actualMinDate(), "Debes ser mayor de 18 años"),
+  password: Yup.string()
+    .required("La contraseña es requerida")
+    .min(8, "La contraseña debe tener al menos 8 caracteres"),
+  repitPass: Yup.string()
+    .required("Requerido")
+    .oneOf([Yup.ref("password")], "Las contraseñas deben coincidir"),
+});
+
+//#region Component
 function RegistroUsuario({ Condicions }) {
-  const [errMsgpass, setErrMsgpass] = useState("");
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
 
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    dateOfBirth: "",
-    password: "",
-    gender: "Masculino",
-    phone: "",
-  });
+  const { getRootProps, getInputProps, isDragActive, acceptedFiles } =
+    useDropzone({ onDrop });
 
-  const classNames = {
-    innerWrapper: "w-[300px]",
-    inputWrapper: "border-none !bg-zinc-100 my-5",
+  const registerResponse = async (values) => {
+    await registerUserForm(values);
   };
 
-  const registerResponse = async () => {
-    await registerUserForm(formData);
-  };
-
-  const handleChangeInput = (event) => {
-    const { name, value } = event.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const { password, repitPass } = e.target;
-
-    console.log(formData);
-
+  const handleSubmit = (values) => {
     //se fija si las propiedaes del objeto del estado formdata estan vacias
-    const checkFormData = (formData) =>
-      Object.values(formData).every((value) => value.trim().length != 0);
-
     if (Condicions()) {
-      if (checkFormData(formData)) {
-        const birthDate = new Date(Date.parse(formData.dateOfBirth)); // Convert to Date object
-        const currentDate = new Date(); // Get current date
-        const age = Math.floor(
-          (currentDate.getTime() - birthDate.getTime()) /
-            (1000 * 60 * 60 * 24 * 365),
-        ); // Calculate age in years
-
-        if (age < 18) {
-          toast.error("El Usuario debe ser mayor a 18 años");
-        } else {
-          if (password.value !== repitPass.value) {
-            setErrMsgpass("Las contraseñas no coinciden");
-            toast.error("Las contraseñas no coinciden");
-          } else {
-            setErrMsgpass("");
-            registerResponse();
-          }
-        }
-      } else {
-        toast.error("Completa los campos correctamente");
-      }
+      registerResponse(values);
     } else {
       toast.error("Porfavor acepte los terminos y condiciones");
     }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <CustomInput
-          name="name"
-          placeholder="Nombre completo"
-          type="text"
-          classNames={classNames}
-          errorMessage={
-            !formData.name.length ? "Nombre completo requerido" : ""
-          }
-          onChange={handleChangeInput}
-        />
-        <CustomInput
-          name="phone"
-          value={formData.phone}
-          placeholder="Telefono"
-          type="text"
-          classNames={classNames}
-          errorMessage={!formData.phone.length ? "Telefono es requerido" : ""}
-          onChange={handleChangeInput}
-        />
-        <CustomInput
-          name="email"
-          placeholder="Email"
-          type="email"
-          classNames={classNames}
-          errorMessage={!formData.email.length ? "Email requerido" : ""}
-          onChange={handleChangeInput}
-        />
-        <CustomInput
-          name="dateOfBirth"
-          placeholder="Fecha de nacimiento"
-          type="date"
-          classNames={classNames}
-          errorMessage={
-            !formData.email.length ? "Fecha de nacimiento requerida" : ""
-          }
-          onChange={handleChangeInput}
-        />
-        <select
-          defaultValue="Masculino"
-          name="gender"
-          onChange={handleChangeInput}
-        >
-          <option value="Masculino">Masculino</option>
-          <option value="Femenino">Femenino</option>
-          <option value="Prefiero no responder">Prefiero no responder</option>
-        </select>
-        <CustomInput
-          name="password"
-          placeholder="Contraseña"
-          type="password"
-          classNames={classNames}
-          errorMessage={!formData.password.length ? "Contraseña requerida" : ""}
-          onChange={handleChangeInput}
-        />
-        <CustomInput
-          name="repitPass"
-          placeholder="Repita contraseña"
-          type="password"
-          classNames={classNames}
-          errorMessage={errMsgpass} // Specific error for repeat password
-          onChange={handleChangeInput}
-        />
+    <Formik
+      onSubmit={handleSubmit}
+      initialValues={initialValues}
+      validationSchema={registerSchemaValidation}
+    >
+      {(values, handleChange) => (
+        <Form className="flex flex-col gap-3">
+          <div className="flex flex-col gap-1">
+            <Field
+              className="px-3 py-2 outline-none border-2 border-gray-400 focus:border-primary-500 rounded-md"
+              name="name"
+              type="String"
+              placeholder="Nombre"
+            />
+            <ErrorMessage
+              name="name"
+              component="span"
+              className="text-danger-500"
+            />
+          </div>
 
-        <CustomButton type="submit">Registrarse</CustomButton>
+          <div className="flex flex-col gap-1">
+            <Field
+              className="px-3 py-2 outline-none border-2 border-gray-400 focus:border-primary-500 rounded-md"
+              name="phone"
+              type="string"
+              placeholder="Telefono"
+            />
+            <ErrorMessage
+              name="phone"
+              component="span"
+              className="text-danger-500"
+            />
+          </div>
 
-        <div className="flex flex-row justify-center items-center gap-4 mt-8">
-          <p>¿Ya esta registrado?</p>
-          <CustomButton
-            className="bg-primary-400 min-w-fit !w-fit py-2"
-            as={Link}
-            href="/login"
+          <div className="flex flex-col gap-1">
+            <Field
+              className="px-3 py-2 outline-none border-2 border-gray-400 focus:border-primary-500 rounded-md"
+              name="email"
+              type="email"
+              placeholder="email"
+            />
+            <ErrorMessage
+              name="email"
+              component="span"
+              className="text-danger-500"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <Field
+              className="px-3 py-2 outline-none border-2 border-gray-400 focus:border-primary-500 rounded-md"
+              name="dateOfBirth"
+              type="date"
+              placeholder="Fecha de nacimiento"
+            />
+            <ErrorMessage
+              name="dateOfBirth"
+              component="span"
+              className="text-danger-500"
+            />
+          </div>
+
+          <Select
+            onChange={handleChange}
+            name="gender"
+            aria-labelledby="gender" // Add this line
           >
-            Ingresar
-          </CustomButton>
-        </div>
-      </div>
-    </form>
+            <SelectItem value="Masculino" label="Masculino">
+              Masculino
+            </SelectItem>
+
+            <SelectItem value="Femenino" label="Femenino">
+              Femenino
+            </SelectItem>
+
+            <SelectItem
+              value="Prefiero no responder"
+              label="Prefiero no responder"
+            >
+              Prefiero no responder
+            </SelectItem>
+          </Select>
+
+          <div className="flex flex-col gap-1">
+            <Field
+              className="px-3 py-2 outline-none border-2 border-gray-400 focus:border-primary-500 rounded-md"
+              name="password"
+              type="password"
+              placeholder="contraseña"
+            />
+            <ErrorMessage
+              name="password"
+              component="span"
+              className="text-danger-500"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <Field
+              className="px-3 py-2 outline-none border-2 border-gray-400 focus:border-primary-500 rounded-md"
+              name="repitPass"
+              type="password"
+              placeholder="Repita la contraseñ"
+            />
+            <ErrorMessage
+              name="repitPass"
+              component="span"
+              className="text-danger-500"
+            />
+          </div>
+
+          <Button className="bg-primary-500 text-white font-sans" type="submit">
+            Logearse
+          </Button>
+
+          <div className="flex flex-row justify-center items-center gap-4 mt-8">
+            <p>¿Ya esta registrado?</p>
+            <Button
+              className="bg-primary-500 text-white font-sans"
+              as={Link}
+              href="/login"
+            >
+              Ingresar
+            </Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
