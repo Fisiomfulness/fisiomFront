10bd71236720fbe52ef5db752dcc2d9e6d46f1ef
Jackira01 <s4bu3z0@gmail.com>
2024-04-21 21:21:20 -0500

    new herlper "actualMinDate"

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,20 +1,22 @@
 // * example = I like apples => I like ...
 export const truncateText = (text, maxWords) => {
-  return text.split(' ').slice(0, maxWords).join(' ') + '...';
+  return text.split(" ").slice(0, maxWords).join(" ") + "...";
 };
 
 // * hour:minutes am/pm
 export const hourTime = (timeStamp) => {
   const date = new Date(timeStamp);
-  return new Intl.DateTimeFormat('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
+  return new Intl.DateTimeFormat("en-US", {
+    hour: "numeric",
+    minute: "numeric",
   }).format(date);
 };
 
 // * Works for ISO 8601 and UNIX timestamps
 export const dateFormatter = (timeStamp, hours = true) => {
-  let displayedDate = new Date(typeof timeStamp === 'number' ? timeStamp * 1000 : timeStamp);
+  let displayedDate = new Date(
+    typeof timeStamp === "number" ? timeStamp * 1000 : timeStamp,
+  );
   const today = new Date();
   today.setHours(0, 0, 0, 0); // * For an accurate comparison
   const yesterday = new Date(today);
@@ -22,10 +24,12 @@ export const dateFormatter = (timeStamp, hours = true) => {
 
   if (displayedDate.toLocaleDateString() === today.toLocaleDateString()) {
     // ? Today... show hours? => hour:minute am/pm
-    displayedDate = hours ? hourTime(timeStamp) : 'Today';
-  } else if (displayedDate.toLocaleDateString() === yesterday.toLocaleDateString()) {
+    displayedDate = hours ? hourTime(timeStamp) : "Today";
+  } else if (
+    displayedDate.toLocaleDateString() === yesterday.toLocaleDateString()
+  ) {
     // ? Yesterday
-    displayedDate = 'Yesterday';
+    displayedDate = "Yesterday";
   } else {
     // ? Before yesterday, display the date => day/month/year
     displayedDate = displayedDate.toLocaleDateString();
@@ -34,13 +38,36 @@ export const dateFormatter = (timeStamp, hours = true) => {
   return displayedDate;
 };
 
-export const scrollTo = (elementId, direction = 'top') => {
+export const actualMinDate = () => {
+  // Convertir la fecha actual a un objeto Date
+  const ActualDate = new Date();
+
+  // Calcular los milisegundos por año
+  const millisecondsForYear = 1000 * 60 * 60 * 24 * 365;
+
+  // Restar 18 años en milisegundos a la fecha actual
+  const milliseconds18years = millisecondsForYear * 18;
+  const minimalDateMillisecMillisec =
+    ActualDate.getTime() - milliseconds18years;
+
+  // Convertir los milisegundos de la fecha mínima a un objeto Date
+  const minimalDate = new Date(minimalDateMillisecMillisec);
+
+  // Formatear la fecha mínima a YYYY-MM-DD
+  const minimalDateString = minimalDate.toISOString().slice(0, 10);
+
+  //retorna "yyyy-mm-dd"
+  console.log(minimalDateString);
+  return minimalDateString;
+};
+
+export const scrollTo = (elementId, direction = "top") => {
   const scrollContainer = document.getElementById(elementId);
-  if (scrollContainer && (direction == 'top' || direction == 'bottom')) {
+  if (scrollContainer && (direction == "top" || direction == "bottom")) {
     const scrollOptions = {
-      top: direction === 'top' ? 0 : scrollContainer.scrollHeight,
+      top: direction === "top" ? 0 : scrollContainer.scrollHeight,
       left: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     };
     scrollContainer.scrollTo(scrollOptions);
   }
