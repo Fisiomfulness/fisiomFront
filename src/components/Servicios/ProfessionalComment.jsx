b2335b08169c168b58578452d5f1904f71c99d62
brunosarti83 <brunosarti.bs@gmail.com>
2024-04-25 11:02:14 -0300

    WIP(updatingProfessionalComments)

---
+++ src/components/Servicios/ProfessionalComment.jsx
@@ -0,0 +1,23 @@
+import { User, Link } from "@nextui-org/react";
+import Stars from "./Star";
+
+export default function Comment({ comment }) {
+  return (
+    <div className="bg-primary-50 p-4 rounded-lg hover:bg-[#D8EEF8] md:p-6 lg:p-8">
+      <div className="flex items-center justify-between mb-2">
+        <User
+          name={comment.sender?.name}
+          description={<Stars rating={comment.rating} />}
+          avatarProps={{
+            src: comment.sender?.image,
+          }}
+          className="text-zinc-800 italic"
+        />
+        <Link href="#" color="danger">
+          Reportar
+        </Link>
+      </div>
+      <p>{comment.content}</p>
+    </div>
+  );
+}
