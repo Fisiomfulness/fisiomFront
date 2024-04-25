86bbaba32dd883201d9d1ead2abd48f24906bc17
Kevin Mamani <kevin72309@gmail.com>
2024-04-25 18:13:18 -0300

    getTypes with no cache

--- src/services/types.js
+++ src/services/types.js
@@ -3,6 +3,7 @@ import { BASE_URL } from '@/utils/api';
 export const getTypes = async () => {
   const res = await fetch(`${BASE_URL}/types`, {
     method: 'GET',
+    cache: 'no-cache',
   });
   if (!res.ok) throw new Error('Error fetching types');
   return await res.json();
