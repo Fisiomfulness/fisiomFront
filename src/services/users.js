62a9794745562bd925706e2c1e5f1c307c368ea4
jackira01 <s4bu3z0@gmail.com>
2024-05-04 09:25:52 -0500

    put login and register services on users services

--- src/services/users.js
+++ src/services/users.js
@@ -2,24 +2,75 @@ import { BASE_URL } from '@/utils/api';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-export const login = async (user) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/login`, user, {
+//#region Login
+export const axiosLogin = async (user) => {
+  return toast.promise(
+    axios.post(`${BASE_URL}/login`, user, {
       withCredentials: true,
-    });
-    toast.success('Logeado con exito!');
-    return response;
-  } catch (error) {
-    if (error.response) {
-      toast.error(error.response.data);
-      return false;
-    } else {
-      toast.error(error.message);
-      return false;
-    }
-  }
+    }),
+    {
+      loading: 'Iniciando Sesion...',
+      success: (response) => response.data.message,
+      error: (error) => {
+        if (error.response) {
+          return error.response.data.message;
+        } else {
+          return error.message;
+        }
+      },
+    },
+  );
 };
 
+//#region Register user
+export const axiosRegisterUserForm = async (user) => {
+  return toast.promise(
+    axios.post(`${BASE_URL}/login/register/user`, user, {
+      withCredentials: true,
+    }),
+    {
+      loading: 'Registrandose...',
+      success: (response) => response.data.message,
+      error: (error) => {
+        if (error.response) {
+          return error.response.data.message;
+        } else {
+          return error.message;
+        }
+      },
+    },
+  );
+};
+
+//#region Register professional
+export const axiosRegisterProfessionalForm = async (user) => {
+  return toast.promise(
+    axios.post(`${BASE_URL}/login/register/professional`, user, {
+      withCredentials: true,
+    }),
+    {
+      loading: 'Registrandose...',
+      success: (response) => response.data.message,
+      error: (error) => {
+        if (error.response) {
+          return error.response.data.message;
+        } else {
+          return error.message;
+        }
+      },
+    },
+  );
+};
+
+//#region User Detail
+export const axiosUserDetail = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/users/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 // ? Cookie is httpOnly for more security, this is needed.
 export const httpLogout = async () => {
   const res = await fetch(`${BASE_URL}/logout`, {
