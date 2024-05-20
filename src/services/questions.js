4258d3676d2cfebaeaff52fb013b2b8d4759cfb4
Kevin Mamani <kevin72309@gmail.com>
2024-05-19 23:06:09 -0300

    feat: questions - get/post/put/ services

---
+++ src/services/questions.js
@@ -0,0 +1,38 @@
+import { BASE_URL } from '@/utils/api';
+
+export const getQuestions = async ({
+  offset = 0,
+  limit = 30,
+  specialtyId,
+  search,
+}) => {
+  let query = `?offset=${offset}&limit=${limit}`;
+  if (specialtyId && specialtyId !== "1") query += `&specialtyId=${specialtyId}`;
+  if (search && search != '') query += `&search=${search}`;
+  const res = await fetch(`${BASE_URL}/questions${query}`, {
+    method: 'GET',
+    cache: 'no-cache',
+  });
+  if (!res.ok) throw new Error('Error obteniendo preguntas');
+  return await res.json();
+};
+
+export const createQuestion = async (values) => {
+  const res = await fetch(`${BASE_URL}/questions/create`, {
+    method: 'POST',
+    body: JSON.stringify(values),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (!res.ok) throw new Error('Error creando la pregunta');
+  return await res.json();
+};
+
+export const respondQuestion = async (questionId, values) => {
+  const res = await fetch(`${BASE_URL}/questions/response/${questionId}`, {
+    method: 'PUT',
+    body: JSON.stringify(values),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (!res.ok) throw new Error('Error respondiendo a la pregunta');
+  return await res.json();
+};
