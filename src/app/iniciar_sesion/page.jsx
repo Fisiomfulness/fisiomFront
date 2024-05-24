2bccf3d3f3f30e52836a24955a3e0dfa7c8038cb
Kevin Mamani <kevin72309@gmail.com>
2024-05-24 00:48:38 -0300

    fix: build error

--- src/app/iniciar_sesion/page.jsx
+++ src/app/iniciar_sesion/page.jsx
@@ -1,9 +1,11 @@
 import { Login } from '../../components/Login';
 
-export const Page = () => {
+const Page = () => {
   return (
     <main className="px-auto min-h-screen center bg-primary-400">
       <Login />
     </main>
   );
 };
+
+export default Page;
