f6c7bc86bb28c72ac927873bcfaae443aee6dd52
Kevin Mamani <kevin72309@gmail.com>
2024-05-23 23:50:32 -0300

    style: fix overflow and icon bad positioning

--- src/components/pregunta_experto/QuestionContent.jsx
+++ src/components/pregunta_experto/QuestionContent.jsx
@@ -9,22 +9,24 @@ const QuestionContent = ({ data }) => {
 
   return (
     <div className="flex flex-col gap-2 sm:flex-row sm:gap-5 items-center">
-      <div className="w-full space-y-2 text-start">
-        <p>
+      <div className="w-full space-y-2 text-start overflow-hidden">
+        <div className="break-all">
           <RiQuestionLine size={21} className="text-[#3DAADD] inline" />
-          <span className="font-semibold mx-2 text-secondary-500">
-            Pregunta:
-          </span>
-          {text}
-        </p>
-        {isAnswered ? (
-          <p>
-            <MdChatBubbleOutline size={19} className="text-[#3DAADD] inline" />
-            <span className="font-semibold mx-2 text-secondary-500">
-              Respuesta:
-            </span>
-            {answer?.text}
+          <p className="inline">
+            <b className="font-semibold mx-2 text-secondary-500">Pregunta:</b>
+            {text}
           </p>
+        </div>
+        {isAnswered ? (
+          <div className="break-all">
+            <MdChatBubbleOutline size={19} className="text-[#3DAADD] inline" />
+            <p className="inline">
+              <b className="font-semibold text-secondary-500 mx-2">
+                Respuesta:
+              </b>
+              {answer?.text}
+            </p>
+          </div>
         ) : (
           <ResponseForm questionId={_id} />
         )}
