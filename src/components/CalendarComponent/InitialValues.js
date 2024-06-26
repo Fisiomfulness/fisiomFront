de9607267fe685d56c776ff08a162ae49ce5eca4
jackira01 <s4bu3z0@gmail.com>
2024-06-26 11:57:54 -0500

    calendar config

---
+++ src/components/CalendarComponent/InitialValues.js
@@ -0,0 +1,114 @@
+export const initialEvents = [
+    {
+        id: 1,
+        title: 'Otorrino',
+        specialty: 'Odontologia',
+        pacient: "Jhon Lee",
+        start: new Date("2024-06-26T06:19"),
+        end: new Date("2024-06-26T13:19"),
+        description: 'Cita con el otorrino',
+        color: '#38b0ff',
+    },
+    {
+        id: 2,
+        title: 'Odontologia',
+        specialty: 'odontologia',
+        pacient: "Luisa Perez",
+        start: new Date("2024-06-25T08:19"),
+        end: new Date("2024-06-25T13:19"),
+        description: 'Cita con el Cirujano',
+    },
+    {
+        id: 3,
+        title: 'Nutricionista',
+        specialty: 'nutricionista',
+        pacient: "Fernan Torres",
+        start: new Date("Junio 26, 2024 14:00:00"),
+        end: new Date("Junio 26, 2024 16:00:00"),
+        description: 'Cita con el Odontologo',
+    },
+    {
+        id: 4,
+        title: 'Ocupado',
+        start: new Date("Junio 27, 2024 04:00:00"),
+        end: new Date("Junio 27, 2024 16:00:00"),
+        description: 'Dia civico',
+        color: 'grey',
+    },
+    {
+        id: 5,
+        title: 'Consulta General',
+        specialty: 'consulta-general',
+        pacient: "Luisa Perez",
+        start: new Date("2024-06-25T08:19"),
+        end: new Date("2024-06-25T13:19"),
+        description: 'Cita con el Cirujano',
+    },]
+
+
+export const specialitiesArray = [
+    {
+        key: "consulta-general",
+        label: "Consulta General"
+    },
+    {
+        key: "fisioterapia-traumatologica",
+        label: "Fisioterapia Traumatológica"
+    },
+    {
+        key: "fisioterapia-deportiva",
+        label: "Fisioterapia Deportiva"
+    },
+    {
+        key: "fisioterapia-neurologica",
+        label: "Fisioterapia Neurológica"
+    },
+    {
+        key: "fisioterapia-pediatrico",
+        label: "Fisioterapia Pediátrico"
+    },
+    {
+        key: "fisioterapia-geriatrica",
+        label: "Fisioterapia Geriátrica"
+    },
+    {
+        key: "fisioterapia-ortopedica",
+        label: "Fisioterapia Ortopédica"
+    },
+    {
+        key: "fisioterapia-respiratoria",
+        label: "Fisioterapia Respiratoria"
+    },
+    {
+        key: "fisioterapia-ontologica",
+        label: "Fisioterapia Ontológica"
+    },
+    {
+        key: "masaje-terapeutico",
+        label: "Masaje Terapéutico"
+    },
+    {
+        key: "masaje-descontracturante",
+        label: "Masaje Descontracturante"
+    },
+    {
+        key: "descarga-muscular",
+        label: "Descarga Muscular"
+    },
+    {
+        key: "masaje-shiatsu",
+        label: "Masaje Shiatsu"
+    },
+    {
+        key: "psicologia",
+        label: "Psicología"
+    },
+    {
+        key: "nutricion",
+        label: "Nutrición"
+    },
+    {
+        key: "laboratorio-clinico",
+        label: "Laboratorio Clínico"
+    }
+];
