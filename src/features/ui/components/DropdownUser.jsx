c837f980f418a0d14f020eba3d60a355f1309d4f
Kevin Mamani <kevin72309@gmail.com>
2024-06-27 19:02:56 -0300

    chore: route renamed (edit_profile => editar_perfil)

--- src/features/ui/components/DropdownUser.jsx
+++ src/features/ui/components/DropdownUser.jsx
@@ -17,7 +17,7 @@ import Link from "next/link";
 import { signOut } from "next-auth/react";
 
 const items = [
-  { key: "/user/edit_profile", label: "Editar perfil" },
+  { key: "/user/editar_perfil", label: "Editar perfil" },
   {
     key: "root_mis_servicios",
     label: "Mis servicios",
