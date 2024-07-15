40b44c75b4bf8204e38cf92fd73ecf1f472bb7fe
Kevin Mamani <kevin72309@gmail.com>
2024-07-15 19:49:58 -0300

    feat: professional services page with cards and whatsapp formated redirect

--- src/app/servicios/[detallesId]/precios/page.jsx
+++ src/app/servicios/[detallesId]/precios/page.jsx
@@ -1,15 +1,17 @@
-import ServicioPrecioCard from "@/components/Servicios/ServicioPrecioCard";
-import data from "@/components/Servicios/data/profesionales.json";
-import profesionalFinder from "@/components/Servicios/utils/utils";
+import { getServices } from '@/services/professionals';
+import ServicioPrecioCard from '@/components/Servicios/ServicioPrecioCard';
+import axios from 'axios';
 
+export const dynamic = 'force-dynamic';
 
-const ServicioPrecios = async ({params}) => {
-  const profesional = await profesionalFinder(params, data);
-  
+const ServicioPrecios = async ({ params }) => {
+  const professionalId = params.detallesId;
+  const { services } = await getServices({ professionalId });
+
   return (
-    <div>
-      <ServicioPrecioCard servicios={profesional.servicios} />
-    </div>
+    <section className="w-full flex flex-col my-2 gap-4 grow lg:max-h-[800px] overflow-y-auto">
+      <ServicioPrecioCard services={services} />
+    </section>
   );
 };
 
--- src/components/Servicios/ServicioPrecioCard.jsx
+++ src/components/Servicios/ServicioPrecioCard.jsx
@@ -1,49 +1,64 @@
-"use client";
-import { Card, CardBody, Chip, Button } from "@nextui-org/react";
+'use client';
+import { Button, Card, CardBody, CardHeader, Chip } from '@nextui-org/react';
+import { capitalizeFirstLetter, startWhatsAppChat } from '@/utils/helpers';
 
-const ServicioPrecioCard = ({ servicios }) => {
- 
+// ? Se esta trabajando con el numero de la empresa.
+const REDIRECT_PHONE = 51901294627;
+
+const ServicioPrecioCard = ({ services }) => {
   return (
     <>
-      {servicios.map((servicio, index) => (
-        <div
-          key={index}
-          className="flex flex-col md:flex-row items-center mt-2"
-        >
+      {services.length > 0 ? (
+        services.map((service) => (
           <Card
-            isBlurred
-            className="border-none w-full  bg-background/60 dark:bg-default-100/50 max-w-[1118px] rounded-r-none md:rounded"
-            shadow="sm"
+            key={service._id}
+            isHoverable
+            radius="none"
+            shadow="none"
+            className="bg-[#EBF7FB] p-1 lg:p-3 lg:px-9 hover:!bg-[#D8EEF8] overflow-visible"
           >
-            <CardBody>
-              <div className="grid grid-cols-4 gap-3  items-center ">
-                <div className="flex flex-col col-span-4 lg:col-span-2  gap-3 p-5  ">
-                  <Chip
-                    size="lg"
-                    className="bg-[#36A793] p-2"
-                    variant="faded"
-                  >
-                    <p className="text-large font-bold text-foreground/80 ">
-                      {servicio.nombre}
-                    </p>
-                  </Chip>
-                  <p className="text-medium  lg:w-full">
-                    {servicio.descripcion}
-                  </p>
-                </div>
-                <div className="flex col-span-2 lg:col-span-1 items-center justify-center">
-                  <h2>Precio: {servicio.precio}</h2>
-                </div>
-                <div className="flex lg:basis-1/4  col-span-2 lg:col-span-1 justify-center">
-                  <Button className="p-5" size="lg" color="primary">
-                    RESERVAR CITA
-                  </Button>
-                </div>
-              </div>
+            <CardHeader>
+              <h3 className="m-0 text-sm rounded-full bg-[#64efbce2] px-5 py-1 text-[#164a37e2] uppercase font-semibold break-all tracking-wider line-clamp-3">
+                {service.title}
+              </h3>
+            </CardHeader>
+            <CardBody className="pt-0 grid xl:grid-cols-[2.5fr,0.5fr,1fr] items-center gap-3">
+              <p className="text-justify">{service.description}</p>
+              <p className="text-center font-semibold text-secondary-500 font-sans">{`$ ${service.price}`}</p>
+              <Button
+                color="primary"
+                radius="none"
+                fullWidth
+                className="bg-[#2984AE] uppercase tracking-wide max-w-[300px] mx-auto font-semibold"
+                onPress={() =>
+                  startWhatsAppChat(
+                    REDIRECT_PHONE,
+                    `Hola, me encuentro interesado/a en el servicio "${
+                      service.title
+                    }" proporcionado por el/la profesional "${capitalizeFirstLetter(
+                      service?._professional.name || ''
+                    )}". Por lo tanto, me gustaría conocer los detalles necesarios para programar una cita. Quedo atento/a a su respuesta, saludos.`
+                  )
+                }
+              >
+                Reservar cita
+              </Button>
             </CardBody>
           </Card>
-        </div>
-      ))}
+        ))
+      ) : (
+        <Card
+          radius="none"
+          shadow="none"
+          className="h-full bg-[#EBF7FB] p-3 lg:px-9"
+        >
+          <CardBody>
+            <p className="m-auto text-center text-lg font-semibold tracking-wide text-secondary-400">
+              Este profesional no ha cargado sus servicios
+            </p>
+          </CardBody>
+        </Card>
+      )}
     </>
   );
 };
