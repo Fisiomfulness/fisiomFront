bbacbe24394607dec11f6c94574b4007ee7d5d7b
clau140 <clau.alberdi89@gmail.com>
2024-09-04 15:24:26 -0300

    validation scheme with Zod in contact

---
+++ src/utils/validations/contactSchema.js
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+import { formikZodValidator } from "@/utils/validations";
+
+const contactSchema = z.object({
+  nombre: z.string().trim().min(1, 'El nombre es requerido').max(50, 'Máximo 50 caracteres'),
+  departamento: z.string().min(1, 'El departamento es requerido'),
+  distrito: z.string().min(1, 'El distrito es requerido'),
+  celular: z.string().trim().regex(/^\d+$/, 'Solo se permiten números').min(9, 'Debe tener al menos 9 dígitos'),
+  email: z.string().email('Debe ser un email válido'),
+  descripcion: z.string().optional(),
+});
+
+export { contactSchema };
