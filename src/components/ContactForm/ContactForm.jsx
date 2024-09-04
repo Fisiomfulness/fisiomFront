54967c0bd5ce18e6f270d62d1489d186f9d305f1
clau140 <clau.alberdi89@gmail.com>
2024-09-04 17:08:31 -0300

    formik in the contact form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,33 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { contactSchema } from "@/utils/validations/contactSchema";
+import { formikZodValidator } from "@/utils/validations";
 import Image from "next/image";
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    nombre: "",
-    departamento: "",
-    distrito: "",
-    celular: "",
-    email: "",
-    descripcion: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (values) => {
     const mensaje =
-      `Nombre: ${formData.nombre}%0A` +
-      `Departamento: ${formData.departamento}%0A` +
-      `Distrito: ${formData.distrito}%0A` +
-      `Celular: ${formData.celular}%0A` +
-      `Email: ${formData.email}%0A` +
-      `Descripción del dolor: ${formData.descripcion}`;
+      `Nombre: ${values.nombre}%0A` +
+      `Departamento: ${values.departamento}%0A` +
+      `Distrito: ${values.distrito}%0A` +
+      `Celular: ${values.celular}%0A` +
+      `Email: ${values.email}%0A` +
+      `Descripción del dolor: ${values.descripcion || "N/A"}`;
 
     window.open(`https://wa.me/51901294627?text=${mensaje}`, "_blank");
   };
@@ -41,119 +27,154 @@ const ContactForm = () => {
       <h1 className="text-2xl sm:text-3xl font-bold mb-6 text-center">
         Chatea con un fisioterapeuta
       </h1>
-      <form
+      <Formik
+        initialValues={{
+          nombre: "",
+          departamento: "",
+          distrito: "",
+          celular: "",
+          email: "",
+          descripcion: "",
+        }}
         onSubmit={handleSubmit}
-        className="w-full max-w-md sm:max-w-lg bg-gray-800 p-6 rounded-lg shadow-md"
+        validate={formikZodValidator(contactSchema)}
       >
-        <div className="mb-4">
-          <input
-            type="text"
-            name="nombre"
-            placeholder="¿Cuál es tu Nombre?*"
-            value={formData.nombre}
-            onChange={handleChange}
-            required
-            className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-        </div>
-        <div className="mb-4">
-          <select
-            name="departamento"
-            value={formData.departamento}
-            onChange={handleChange}
-            required
-            className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-          >
-            <option value="">Selecciona Departamento*</option>
-
-            <option value="Amazonas">Amazonas</option>
-            <option value="Ancash">Ancash</option>
-            <option value="Apurímac">Apurímac</option>
-            <option value="Arequipa">Arequipa</option>
-            <option value="Ayacucho">Ayacucho</option>
-            <option value="Cajamarca">Cajamarca</option>
-            <option value="Cusco">Cusco</option>
-            <option value="Huancavelica">Huancavelica</option>
-            <option value="Huánuco">Huánuco</option>
-            <option value="Ica">Ica</option>
-            <option value="Junín">Junín</option>
-            <option value="La Libertad">La Libertad</option>
-            <option value="Lambayeque">Lambayeque</option>
-            <option value="Lima">Lima</option>
-            <option value="Loreto">Loreto</option>
-            <option value="Madre de Dios">Madre de Dios</option>
-            <option value="Moquegua">Moquegua</option>
-            <option value="Pasco">Pasco</option>
-            <option value="Piura">Piura</option>
-            <option value="Puno">Puno</option>
-            <option value="San Martín">San Martín</option>
-            <option value="Tacna">Tacna</option>
-            <option value="Tumbes">Tumbes</option>
-            <option value="Ucayali">Ucayali</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="distrito"
-            placeholder="Distrito*"
-            value={formData.distrito}
-            onChange={handleChange}
-            required
-            className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="celular"
-            placeholder="Celular con WhatsApp*"
-            value={formData.celular}
-            onChange={handleChange}
-            required
-            className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="email"
-            name="email"
-            placeholder="Email*"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-          />
-        </div>
-        <div className="mb-4">
-          <textarea
-            name="descripcion"
-            placeholder="Describe tu dolor (opcional)"
-            value={formData.descripcion}
-            onChange={handleChange}
-            className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
-          ></textarea>
-        </div>
-        <div className="mb-4 flex items-center">
-          <input type="checkbox" required className="mr-2" />
-          <span>
-            He leído y acepto la{" "}
-            <a href="/politica-privacidad" className="text-green-500 underline">
-              Política de Privacidad
-            </a>
-          </span>
-        </div>
-        <button
-          type="submit"
-          className="w-full p-3 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300 ease-in-out"
-        >
-          Chatear
-        </button>
-      </form>
+        {() => (
+          <Form className="w-full max-w-md sm:max-w-lg bg-gray-800 p-6 rounded-lg shadow-md">
+            <div className="mb-4">
+              <Field
+                type="text"
+                name="nombre"
+                placeholder="¿Cuál es tu Nombre?*"
+                className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <ErrorMessage
+                name="nombre"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div className="mb-4">
+              <Field
+                as="select"
+                name="departamento"
+                className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+              >
+                <option value="">Selecciona Departamento*</option>
+                <option value="Amazonas">Amazonas</option>
+                <option value="Ancash">Ancash</option>
+                <option value="Apurímac">Apurímac</option>
+                <option value="Arequipa">Arequipa</option>
+                <option value="Ayacucho">Ayacucho</option>
+                <option value="Cajamarca">Cajamarca</option>
+                <option value="Cusco">Cusco</option>
+                <option value="Huancavelica">Huancavelica</option>
+                <option value="Huánuco">Huánuco</option>
+                <option value="Ica">Ica</option>
+                <option value="Junín">Junín</option>
+                <option value="La Libertad">La Libertad</option>
+                <option value="Lambayeque">Lambayeque</option>
+                <option value="Lima">Lima</option>
+                <option value="Loreto">Loreto</option>
+                <option value="Madre de Dios">Madre de Dios</option>
+                <option value="Moquegua">Moquegua</option>
+                <option value="Pasco">Pasco</option>
+                <option value="Piura">Piura</option>
+                <option value="Puno">Puno</option>
+                <option value="San Martín">San Martín</option>
+                <option value="Tacna">Tacna</option>
+                <option value="Tumbes">Tumbes</option>
+                <option value="Ucayali">Ucayali</option>
+              </Field>
+              <ErrorMessage
+                name="departamento"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div className="mb-4">
+              <Field
+                type="text"
+                name="distrito"
+                placeholder="Distrito*"
+                className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <ErrorMessage
+                name="distrito"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div className="mb-4">
+              <Field
+                type="text"
+                name="celular"
+                placeholder="Celular con WhatsApp*"
+                className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <ErrorMessage
+                name="celular"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div className="mb-4">
+              <Field
+                type="email"
+                name="email"
+                placeholder="Email*"
+                className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div className="mb-4">
+              <Field
+                as="textarea"
+                name="descripcion"
+                placeholder="Describe tu dolor (opcional)"
+                className="w-full p-3 rounded bg-gray-700 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <ErrorMessage
+                name="descripcion"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <div className="mb-4 flex items-center">
+              <Field type="checkbox" name="acceptTerms" className="mr-2" />
+              <span>
+                He leído y acepto la{" "}
+                <a
+                  href="/politica-privacidad"
+                  className="text-green-500 underline"
+                >
+                  Política de Privacidad
+                </a>
+              </span>
+              <ErrorMessage
+                name="acceptTerms"
+                component="div"
+                className="text-red-500"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full p-3 bg-green-500 text-white rounded hover:bg-green-600 transition duration-300 ease-in-out"
+            >
+              Chatear
+            </button>
+          </Form>
+        )}
+      </Formik>
       <div className="mt-6 text-center">
         <p className="mb-4">
           O podrías llamarnos haciendo directamente click aquí
         </p>
+
         <button onClick={handleCallClick} className="hover:opacity-80">
           <Image src="/call-icon.png" alt="Llamar" width={48} height={48} />
         </button>
