e1842a5519413e35cd027e6d39879a00ad323967
clau140 <clau.alberdi89@gmail.com>
2024-10-02 21:26:56 -0300

    Professional account approved by the administrator

---
+++ src/app/PendingProfessionals/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PendingProfessionals from "../../components/Dashboard/ProfessionalViewPending/PendingProfessionals";
+
+const PendingProfessionalsPage = () => {
+  return (
+    <div>
+      <PendingProfessionals />
+    </div>
+  );
+};
+
+export default PendingProfessionalsPage;
